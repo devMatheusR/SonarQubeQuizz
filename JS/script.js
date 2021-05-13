cde4c86af6d3a7951baf7b458843fd5f69b3ba1e
Wuors <gustaano881@gmail.com>
2021-05-12 21:54:35 -0300

    Tela de carregamento

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,6 @@
 var carregar = setInterval(function(){
-    clearInterval(carregar, )
-})
+    clearInterval(carregar);
+
+    document.getElementById("imagem-inicial").style.display = "none";
+    document.getElementById("nome").style.display = "inline";
+},4000)
