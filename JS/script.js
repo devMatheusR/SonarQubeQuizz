c46825aadc69ea4c0bd16ff62cc255f134341e90
Wuors <gustaano881@gmail.com>
2021-05-13 14:03:17 -0300

    Tela inicial pronta

--- JS/script.js
+++ JS/script.js
@@ -2,5 +2,5 @@ var carregar = setInterval(function(){
     clearInterval(carregar);
 
     document.getElementById("imagem-inicial").style.display = "none";
-    document.getElementById("nome").style.display = "inline";
-},4000)
+    document.getElementById("tela-oculta").style.display = "inline";
+},3500);
